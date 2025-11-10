263e31147078c9ec7972a653942ceb2eb58fa901
F4RAN <parsa.hassani30@gmail.com>
2025-11-10 19:30:07 +0330

    feat(main): add application bootstrap and dependency wiring

--- node/main.ts
+++ node/main.ts
@@ -4,12 +4,20 @@ import { CreateUser } from "./application/usecases/CreateUser";
 import { createUserController } from "./presentation/http/CreateUserController.js";
 import { MongoClient } from "mongodb";
 
+async function start() {
+    const mongo = new MongoClient("mongodb://localhost:27017");
+    await mongo.connect();
+    const mongodb = mongo.db("Mini-CC");
+    
+    const repo = new MongoUserRepository(mongodb);
+    const createUser = new CreateUser(repo);
+    
+    const app = express();
+    app.use(express.json());
+    app.post("/create_user", createUserController(createUser));
+    app.listen(3000, () => {
+        console.log("Server running on http://localhost:3000");
+    });
+}
 
-const mongo = new MongoClient();
-const repo = new MongoUserRepository(mongo);
-const createDeal = new CreateUser(repo);
-
-const app = express();
-app.use(express.json());
-app.post("/create_user", createUserController(createDeal));
-app.listen(3000);
+start().catch(console.error);
